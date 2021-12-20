f6f6092f0b848e4194434814041daacf3780bb2a
HangSub999 <gkdtjq999@gmail.com>
2021-12-20 18:29:01 +0900

    21.12.20

--- src/Baek10871.java
+++ src/Baek10871.java
@@ -3,22 +3,27 @@ public class Baek10871 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int x = sc.nextInt();
+        int a = sc.nextInt();
         int b = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
 
-            if (x<b) {
-                if (num>x)
+            if (a<b){
+                if (num>a)
                     if (num<b)
-                System.out.print(num + " ");
+                    System.out.print(num+" ");
+            }else if (a>b){
+                if (num<a)
+                    if (num>b)
+                        System.out.print(num+" ");
+            }else
+              System.out.println("값이 없습니다.");
+            break;
 
-            }else {
-                System.out.println(num+" ");
             }
 
         }
     }
-}
+
 
