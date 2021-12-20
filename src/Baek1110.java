28d5c8f3ceee1fc8a961bf3090fd9d2af1d3a029
HangSub999 <gkdtjq999@gmail.com>
2021-12-20 13:32:15 +0900

    21.12.20

---
+++ src/Baek1110.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Baek1110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt=0;
+        int copy=n;
+
+        do {
+            n = ((n%10)*10)+(((n/10)+(n%10))%10);
+            cnt++;
+
+            }while (copy != n);
+        System.out.println(cnt);
+        }
+    }
+
