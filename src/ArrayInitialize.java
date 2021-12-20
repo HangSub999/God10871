ae13ccbd9449807e262d168e5838a37a259d0adc
HangSub999 <gkdtjq999@gmail.com>
2021-12-20 15:57:50 +0900

    21.12.20

---
+++ src/ArrayInitialize.java
@@ -0,0 +1,8 @@
+public class ArrayInitialize {
+    public static void main(String[] args) {
+        ArrayInitialize array=new ArrayInitialize();
+    }
+    public void oterInit(){
+        int [] lottoNumbers={1,4,6,22,23,55,21};
+    }
+}
