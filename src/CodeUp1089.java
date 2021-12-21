733713dc595fc2cb9d7fbd5fac4d0d5c8a6173d6
HangSub999 <gkdtjq999@gmail.com>
2021-12-21 18:03:26 +0900

    21.12.21

--- src/CodeUp1089.java
+++ src/CodeUp1089.java
@@ -5,8 +5,7 @@ public class CodeUp1089 {
         int a= sc.nextInt();
         int d= sc.nextInt();
         int n= sc.nextInt();
-        int sum=0;
-        int sum2=0;
+
         for (int i=2;i<=n;i++){
             a=a+d;
             if (i==n){
