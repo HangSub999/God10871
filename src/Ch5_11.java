647557f31083abad715730c810b55f6c1cfd5f71
HangSub999 <gkdtjq999@gmail.com>
2021-12-22 16:30:57 +0900

    21.12.22

---
+++ src/Ch5_11.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Ch5_11 {        //랜덤 가위,바위,보 만들기
+    public static void main(String[] args) {
+        String [] strArr = {"가위","바위","보"};
+        int tmp=0;
+
+        for (int i=0;i<10;i++){
+            tmp= (int) (Math.random()*3);
+            System.out.println(strArr[tmp]);
+        }
+
+    }
+}
