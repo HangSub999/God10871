49dc82676fdcbb021a9e8539dc27c80f37191594
HangSub999 <gkdtjq999@gmail.com>
2021-12-22 22:03:51 +0900

    21.12.22

---
+++ src/CodeUp1173.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CodeUp1173 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int t= sc.nextInt();
+        int m= sc.nextInt();
+
+       if (m<30){
+           --t;
+           m=60-(30-m);
+           if (t<0){
+               t =23;
+           }
+           System.out.println(t+" "+m);
+       }else {
+           System.out.println(t+" "+(m-30));
+       }
+
+    }
+}
