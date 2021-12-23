94c8e4d096dc741ecc5288b58cc34ec91ae8926e
HangSub999 <gkdtjq999@gmail.com>
2021-12-23 19:08:16 +0900

    21.12.22

---
+++ src/Baek10101.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Baek10101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int sum = a + b + c;
+        if (a == 60 && b == 60 && c == 60) {
+            System.out.println("Equilateral");
+        } else if (sum == 180) {
+            if ((a == b || a == c) || b == c) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        } else {
+            System.out.println("Error");
+        }
+
+    }
+}
