2ae3b6fd376c077d3b8970bcfeab85bde9296802
HangSub999 <gkdtjq999@gmail.com>
2021-12-27 01:14:27 +0900

    21.12.27

--- src/Baek2577.java
+++ src/Baek2577.java
@@ -1,12 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Baek2577 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int a= sc.nextInt();
-        int b= sc.nextInt();
-        int c= sc.nextInt();
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int sum=a*b*c;
+        int [] arr = {10};
+
+
+       while (sum<0){
+           sum %= 10;
+           if (arr[0] == sum){
+
+           }
+       }
+
 
 
     }
