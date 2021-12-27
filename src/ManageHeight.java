291e1229b248450a461034d65cb9e1c535a9815b
HangSub999 <gkdtjq999@gmail.com>
2021-12-27 17:29:45 +0900

    21.12.27

---
+++ src/ManageHeight.java
@@ -0,0 +1,20 @@
+public class ManageHeight {
+    public static void main(String[] args) {
+        ManageHeight mh=new ManageHeight();
+
+        mh.setData();
+
+    }
+    public void setData(){
+        int [][] gradeHeights = {
+                {170,180,173,175,177}
+                ,{160,165,167,186}
+                ,{158,177,187,176}
+                ,{173,182,181}
+                ,{170,180,165,177,172}
+        };
+    }
+    public void printHeight(int classNo){
+        for (int i=0;i<)
+    }
+}
