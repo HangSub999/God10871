65f878cf7223e0410705c1484f3af09075106439
HangSub999 <gkdtjq999@gmail.com>
2021-12-28 22:08:16 +0900

    21.12.28

---
+++ src/Baek2445.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Baek2445 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int k = (n * 2) - (i + i); k > 0; k--) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < i; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("*");
+            }
+            for (int k = (i * 2); k > 0; k--) {
+                System.out.print(" ");
+            }
+            for (int s = i; s < n; s++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
