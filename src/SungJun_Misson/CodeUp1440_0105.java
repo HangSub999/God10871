957346922acc2f6f207a61449688916df2e3a24d
HangSub999 <gkdtjq999@gmail.com>
2022-01-05 22:10:49 +0900

    22.01.05

---
+++ src/SungJun_Misson/CodeUp1440_0105.java
@@ -0,0 +1,31 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class CodeUp1440_0105 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arrN = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arrN[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(i + 1 + ": ");
+            for (int j = 0; j < n; j++) {
+                if (arrN[i] < arrN[j] && i != j) {
+                    System.out.print("< ");
+                } else if (arrN[i] > arrN[j] && i != j) {
+                    System.out.print("> ");
+                } else if (arrN[i] == arrN[j] && i != j) {
+                    System.out.print("= ");
+                }
+
+
+            }
+            System.out.println();
+
+        }
+    }
+}
