b56ea55c2d174e927dae52d30ebc5c5366aac61a
HangSub999 <gkdtjq999@gmail.com>
2022-01-06 13:32:58 +0900

    22.01.06

--- src/SungJun_Misson/CodeUp1414_0105.java
+++ src/SungJun_Misson/CodeUp1414_0105.java
@@ -5,27 +5,35 @@ import java.util.Scanner;
 public class CodeUp1414_0105 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        str.toLowerCase();
-        char[] arr = str.toCharArray();
-
+        String str = sc.nextLine(); // 문자열 입력
+        String str2 = str.toLowerCase(); // 입력받은 문자열 소문자로 변환
+        char[] arr = str2.toCharArray(); //변환된 문자열을 문자형 배열로 변환
+        boolean cc = str.contains("cc");
+        boolean cC = str.contains("cC");
+        boolean Cc = str.contains("Cc");
+        boolean CC = str.contains("CC");
         int count = 0;
         int count2 = 0;
-        String[] cc = new String[arr.length - 1];
 
-
+        //풀지 못함
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                cc[i] = String.valueOf(arr[i] + arr[i + 1]);
-            }
-
             if (arr[i] == 'c') {
                 count++;
-            } else if (cc[i] == "cc") {
-                count2++;
             }
         }
+        for (int i = 1; i < str.length(); i++) {
+            if (cc) {
+                count2++;
+            } else if (cC) {
+                count2++;
+            } else if (Cc) {
+                count2++;
+            } else if (CC) {
+                count2++;
+            }
+
+        }
         System.out.println(count);
-        System.out.println(count2);
+        System.out.print(count2);
     }
 }
