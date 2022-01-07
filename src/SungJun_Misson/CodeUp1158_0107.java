0d64282ac9711c0c64ff0df65386de5b0b850671
HangSub999 <gkdtjq999@gmail.com>
2022-01-07 23:09:30 +0900

    22.01.07

---
+++ src/SungJun_Misson/CodeUp1158_0107.java
@@ -0,0 +1,16 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class CodeUp1158_0107 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 40 && 40 >= n || n >= 60 && 70 >= n) {
+            System.out.println("win");
+        } else {
+            System.out.println("lose");
+        }
+    }
+}
