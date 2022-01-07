11efe76080c512e79f8f582b221f66ff29f7e727
HangSub999 <gkdtjq999@gmail.com>
2022-01-07 23:37:37 +0900

    22.01.07

---
+++ src/SungJun_Misson/CodeUp1218_0107.java
@@ -0,0 +1,27 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class CodeUp1218_0107 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if (a <= b && b <= c) {
+
+            if (a == b && b == c) {
+                System.out.println("정삼각형");
+            } else if (a == b || b == c) {
+                System.out.println("이등변삼각형");
+            } else if ((a * a) + (b * b) == c * c) {
+                System.out.println("직각삼각형");
+            } else {
+                System.out.println("삼각형");
+            }
+        } else {
+            System.out.println("삼각형아님");
+        }
+    }
+}
