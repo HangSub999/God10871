fd85e0991141045210d0811de88c89696bdfad39
HangSub999 <gkdtjq999@gmail.com>
2022-01-07 23:10:27 +0900

    22.01.07

--- src/SungJun_Misson/CodeUp1158_0107.java
+++ src/SungJun_Misson/CodeUp1158_0107.java
@@ -7,7 +7,7 @@ public class CodeUp1158_0107 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n >= 40 && 40 >= n || n >= 60 && 70 >= n) {
+        if (n >= 30 && 40 >= n || n >= 60 && 70 >= n) {
             System.out.println("win");
         } else {
             System.out.println("lose");
