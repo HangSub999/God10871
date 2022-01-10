6a7ffa2b8ad1b16a0d720ee4b12b33d3e440472e
HangSub999 <gkdtjq999@gmail.com>
2022-01-10 19:57:10 +0900

    22.01.10

--- src/SungJun_Misson/Baek0109_1259.java
+++ src/SungJun_Misson/Baek0109_1259.java
@@ -5,11 +5,32 @@ import java.util.Scanner;
 public class Baek0109_1259 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arrN = new int[n];
-        int s = 1;
-        while (s != 0) {
 
+        while (true) {
+            String N = sc.next();
+            if (N.equals("0")) {
+                System.exit(0);
+            }
+            String[] arr = new String[N.length()];
+            String[] arr2 = new String[N.length()];
+
+            for (int i = 0; i < N.length(); i++) {
+                arr[i] = N.substring(i, i + 1);
+            }
+            for (int i = 0; i < N.length(); i++) {
+                arr2[i] = arr[N.length() - 1 - i];
+            }
+            int cont = 0;
+            for (int i = 0; i < N.length(); i++) {
+                if (arr[i].equals(arr2[i])) {
+                    cont++;
+                }
+            }
+            if (cont == N.length()) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
         }
     }
 }
---
+++ src/SungJun_Misson/Baek0110_1264.java
@@ -0,0 +1,31 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0110_1264 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true) {
+            String str = sc.nextLine();
+            String str2 = str.toLowerCase();
+            if (str.equals("#")) {
+                break;
+            }
+            int cnt = 0;
+            String[] arrStr = {"a", "e", "i", "o", "u"};
+
+            for (int i = 0; i < arrStr.length; i++) {
+                for (int j = 0; j < str.length(); j++) {
+                    if (arrStr[i].equals(str2.substring(j, j + 1))) {
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
+
+        }
+
+    }
+}
