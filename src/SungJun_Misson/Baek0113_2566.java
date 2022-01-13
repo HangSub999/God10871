9bbede543d7296c9190e88f802aaae6ae4534933
HangSub999 <gkdtjq999@gmail.com>
2022-01-14 01:18:26 +0900

    22.01.13

--- src/SungJun_Misson/Baek0113_2566.java
+++ src/SungJun_Misson/Baek0113_2566.java
@@ -1,10 +1,38 @@
 package SungJun_Misson;
 
+
 import java.util.Scanner;
 
+//내일 다시 풀어야함
 public class Baek0113_2566 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int[][] N = new int[9][9];
+        int max = 1;
+        int I = 0;
+        int J = 0;
+        for (int i = 0; i < N.length; i++) {
+            for (int j = 0; j < N.length; j++) {
+                N[i][j] = sc.nextInt();
+            }
+
+        }
+        for (int i = 0; i < N.length; i++) {
+            for (int j = 0; j < N.length; j++) {
+                if (max > N[i][j])
+                    max = (Math.max(N[i][j], N[N.length - 1][N.length - 1]));
+            }
+        }
+        for (int i = 0; i < N.length; i++) {
+            for (int j = 0; j < N.length; j++) {
+                if (max == N[i][j]) {
+                    I = i;
+                    J = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(I + " " + J);
 
     }
 }
