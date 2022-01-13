1cb29f1236486655666986b98f112f81aa0ea737
HangSub999 <gkdtjq999@gmail.com>
2022-01-14 00:34:14 +0900

    22.01.13

---
+++ src/SungJun_Misson/Baek0113_2476.java
@@ -0,0 +1,36 @@
+package SungJun_Misson;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baek0113_2476 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //답지 참고함 머리 깨짐
+        int N = sc.nextInt();
+        int[][] dice = new int[N][3];
+        int[] sum = new int[N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 3; j++) {
+                dice[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (dice[i][0] == dice[i][1] && dice[i][1] == dice[i][2] && dice[i][0] == dice[i][2]) {
+                sum[i] = 10000 + (dice[i][0] * 1000);
+            } else if (dice[i][0] == dice[i][1] || dice[i][0] == dice[i][2] || dice[i][1] == dice[i][2]) {
+                sum[i] = 1000 + 100 * (dice[i][0] == dice[i][1] ? dice[i][0] : dice[i][2]);
+            } else {
+                if (dice[i][0] > dice[i][1] && dice[i][0] > dice[i][2]) {
+                    sum[i] = dice[i][0] * 100;
+                } else if (dice[i][1] > dice[i][0] && dice[i][1] > dice[i][2]) {
+                    sum[i] = dice[i][1] * 100;
+                } else if (dice[i][2] > dice[i][0] && dice[i][2] > dice[i][1]) {
+                    sum[i] = dice[i][2] * 100;
+                }
+            }
+        }
+        Arrays.sort(sum);
+        System.out.println(sum[sum.length - 1]);
+    }
+}
