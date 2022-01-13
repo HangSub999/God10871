bb12742f89caa3d17de407e22cd11d42f66bd904
HangSub999 <gkdtjq999@gmail.com>
2022-01-13 23:06:00 +0900

    22.01.13

---
+++ src/SungJun_Misson/Baek0113_3004.java
@@ -0,0 +1,20 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0113_3004 {
+    public static void main(String[] args) { //수학적 사고가 돌아가지 않아 답지 참고
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int piece = 1;
+        int a = 1;
+        for (int i = 0; i < N; i++) {
+            if (i % 2 != 0) {
+                a += 1;
+            }
+            piece += a;
+        }
+        System.out.println(piece);
+    }
+}
