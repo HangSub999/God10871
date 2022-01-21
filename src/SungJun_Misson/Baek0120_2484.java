ded5d5e3920ee78526579e93f4d492b6fa8dc2b8
HangSub999 <gkdtjq999@gmail.com>
2022-01-21 23:52:52 +0900

    22.01.21

--- src/SungJun_Misson/Baek0120_2484.java
+++ src/SungJun_Misson/Baek0120_2484.java
@@ -6,20 +6,22 @@ public class Baek0120_2484 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int[][] arrN = new int[N][4];
-        int[] sum = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 4; j++) {
-                arrN[i][j] = sc.nextInt();
+        int[] dice = new int[7];
+        int[] a = new int[N];
+        int sum = 0;
+        int cont = 0;
+        while (cont > 5) {
+            for (int i = 0; i < N; i++) {
+                a[i] = sc.nextInt();
             }
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (arrN[i][j] == arrN[i][j]) {
-                    sum[i] = 65000;
+            for (int i = 0; i < N; i++) {
+                for (int j = 1; j < 7; j++) {
+                    if (a[i] == j) {
+                        dice[j]++;
+                    }
                 }
             }
+
         }
     }
 }
