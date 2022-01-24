e5928e33bb7bfe50e12af81fb00601d3012e8c84
HangSub999 <gkdtjq999@gmail.com>
2022-01-24 17:36:40 +0900

    22.01.24

---
+++ src/SungJun_Misson/Baek0124_4153.java
@@ -0,0 +1,29 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0124_4153 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            } else if (a * a == (b * b) + (c * c) ||    //직삼각형 구하는 식만 참고
+                    b * b == (a * a) + (c * c) ||
+                    c * c == (b * b) + (a * a)) {
+
+                System.out.println("right");
+
+            } else {
+                System.out.println("wrong");
+            }
+
+
+        }
+    }
+}
