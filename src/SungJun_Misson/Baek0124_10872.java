7392f2754297c553a22d1f9bf3b93ca2e839432b
HangSub999 <gkdtjq999@gmail.com>
2022-01-24 16:10:57 +0900

    22.01.24

---
+++ src/SungJun_Misson/Baek0124_10872.java
@@ -0,0 +1,20 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+//문제를 이해하지 못해 답지 참고
+public class Baek0124_10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int sum = 1;
+
+        while (N != 0) {
+            sum = sum * N;
+            N--;
+        }
+        System.out.println(sum);
+    }
+}
