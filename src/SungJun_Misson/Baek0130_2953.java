4890d35be60180454adf3f8493901cfa92569fa0
HangSub999 <gkdtjq999@gmail.com>
2022-01-30 19:28:07 +0900

    22.01.30

---
+++ src/SungJun_Misson/Baek0130_2953.java
@@ -0,0 +1,32 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0130_2953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] participant = new int[5][4];
+        int[] score = new int[5];
+        int max = 0;
+        int maxNum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                participant[i][j] = sc.nextInt();
+                score[i] += participant[i][j];
+            }
+            if (score[i] > max) {
+                max = score[i];
+                maxNum = i + 1;
+            }
+        }
+
+        System.out.println(maxNum + " " + max);
+
+
+    }
+
+
+}
+
