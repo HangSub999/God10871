b2922a5a0efbc1f5e1fa94a8f61a2611537ec5d0
HangSub999 <gkdtjq999@gmail.com>
2022-02-02 23:47:26 +0900

    22.02.02

---
+++ src/SungJun_Misson/Baek0202_10824.java
@@ -0,0 +1,22 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0202_10824 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] abcd = new String[4];
+
+        for (int i = 0; i < 4; i++) {
+            abcd[i] = sc.next();
+        }
+
+        String ab = abcd[0] + abcd[1];
+        String cd = abcd[2] + abcd[3];
+
+        long abl = Long.parseLong(ab);
+        long cdl = Long.parseLong(cd);
+
+        System.out.println(abl + cdl);
+    }
+}
