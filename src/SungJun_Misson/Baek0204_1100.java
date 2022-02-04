a4e8b45162bdee333a791e33d86df65ea47d8c19
HangSub999 <gkdtjq999@gmail.com>
2022-02-04 18:03:18 +0900

    22.02.04

---
+++ src/SungJun_Misson/Baek0204_1100.java
@@ -0,0 +1,11 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0204_1100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
