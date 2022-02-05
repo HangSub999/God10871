78a867b2b9b913f2dd5cfc654f65f58dbe16ea30
HangSub999 <gkdtjq999@gmail.com>
2022-02-05 15:17:46 +0900

    22.02.05 ArrayList 사용하여 문제 풀기

--- src/Baek2562.java
+++ src/Baek2562.java
@@ -1,20 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Baek2562 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[9];
+        ArrayList<Integer> arrN = new ArrayList<Integer>(9);
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < 9; i++) {
+            arrN.add(sc.nextInt());
         }
         int max = 0;
         int cont = 0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < 9; i++) {
 
-            if (arr[i] > max) {
-                max = arr[i];
+            if (arrN.get(i) > max) {
+                max = arrN.get(i);
                 cont = i + 1;
             }
         }
