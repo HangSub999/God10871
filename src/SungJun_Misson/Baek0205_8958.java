c4e671dcb2746f79b2d14cd3bb9358290838cc84
HangSub999 <gkdtjq999@gmail.com>
2022-02-05 16:16:17 +0900

    22.02.05 ArrayList 사용하여 문제 풀기

---
+++ src/SungJun_Misson/Baek0205_8958.java
@@ -0,0 +1,16 @@
+package SungJun_Misson;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Baek0205_8958 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCas = sc.nextInt();
+        ArrayList<String> arrS = new ArrayList<String>();
+
+        for (int i = 0; i < testCas; i++) {
+            arrS.add(sc.next());
+        }
+    }
+}
