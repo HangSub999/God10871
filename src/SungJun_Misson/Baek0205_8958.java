3fc735b9c825904dee1b60d50d1d3e9a3df13a72
HangSub999 <gkdtjq999@gmail.com>
2022-02-06 22:35:45 +0900

    22.02.06

--- src/SungJun_Misson/Baek0205_8958.java
+++ src/SungJun_Misson/Baek0205_8958.java
@@ -7,10 +7,18 @@ public class Baek0205_8958 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCas = sc.nextInt();
-        ArrayList<String> arrS = new ArrayList<String>();
+        String str;
+        int[] count;
 
         for (int i = 0; i < testCas; i++) {
-            arrS.add(sc.next());
+            str = sc.next();
+            count = new int[str.length()];
+            for (int j = 0; j < str.length(); j++) {
+                char cha = str.charAt(j);
+                if (cha == 'O') {
+                    count[j]++;
+                }
+            }
         }
     }
 }
