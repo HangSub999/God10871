e1f2dfd54596333b72e83df5d2b18f31460752cc
HangSub999 <gkdtjq999@gmail.com>
2022-02-11 23:59:17 +0900

    22.02.11

--- src/SungJun_Misson/Baek0211_2908.java
+++ src/SungJun_Misson/Baek0211_2908.java
@@ -7,10 +7,30 @@ public class Baek0211_2908 {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
+        int[] intA = new int[a.length()];
+        int[] intB = new int[a.length()];
+        int sumA = 0;
+        int sumB = 0;
 
         for (int i = a.length() - 1; i >= 0; i--) {
-            char cha = a.charAt(i);
-            
+            intA[i] = Character.getNumericValue(a.charAt(i));
+            intB[i] = Character.getNumericValue(b.charAt(i));
+
+            if (i == 2) {
+                intA[i] = intA[i] * 100;
+                intB[i] = intB[i] * 100;
+            } else if (i == 1) {
+                intA[i] = intA[i] * 10;
+                intB[i] = intB[i] * 10;
+            }
+            sumA += intA[i];
+            sumB += intB[i];
         }
+        if (sumA > sumB) {
+            System.out.println(sumA);
+        } else {
+            System.out.println(sumB);
+        }
+
     }
 }
