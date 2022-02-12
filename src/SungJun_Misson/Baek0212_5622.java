d644b44d87a90f5fb6183352da09267000ea780c
HangSub999 <gkdtjq999@gmail.com>
2022-02-12 15:28:28 +0900

    22.02.12

---
+++ src/SungJun_Misson/Baek0212_5622.java
@@ -0,0 +1,25 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0212_5622 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String[] maseg = {" ", " ", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ",};
+        int timeOne = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = 0; j < maseg.length; j++) {
+                for (int k = 0; k < maseg[j].length(); k++) {
+                    if (str.charAt(i) == maseg[j].charAt(k)) {
+                        timeOne += j + 1;
+
+                    }
+
+                }
+            }
+        }
+        System.out.println(timeOne);
+    }
+}
