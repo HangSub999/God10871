34a4ecde59d113397055557afc15f7cec72b21c2
HangSub999 <gkdtjq999@gmail.com>
2022-02-13 23:17:46 +0900

    22.02.13

---
+++ src/SungJun_Misson/Baek_0213_15596.java
@@ -0,0 +1,14 @@
+package SungJun_Misson;
+
+class Test {
+    long sum(int[] a) {
+        long sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
+
+
