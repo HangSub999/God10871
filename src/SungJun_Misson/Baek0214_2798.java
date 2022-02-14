36fd95c53727a6b13a9aff4c66f72eed3494f071
HangSub999 <gkdtjq999@gmail.com>
2022-02-14 22:38:46 +0900

    22.02.14

---
+++ src/SungJun_Misson/Baek0214_2798.java
@@ -0,0 +1,21 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0214_2798 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arrN = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arrN[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+            }
+        }
+    }
+}
