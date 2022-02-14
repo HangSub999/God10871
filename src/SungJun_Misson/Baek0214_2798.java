8502716839a68a77d39bdbea46ebd3ba8b927146
HangSub999 <gkdtjq999@gmail.com>
2022-02-14 23:55:31 +0900

    22.02.14

--- src/SungJun_Misson/Baek0214_2798.java
+++ src/SungJun_Misson/Baek0214_2798.java
@@ -21,9 +21,9 @@ public class Baek0214_2798 {
 
         for (int i = 0; i < N - 2; i++) {
 
-            for (int j = 0; j < N - 1; j++) {
+            for (int j = i + 1; j < N - 1; j++) {
 
-                for (int k = 0; k < N; k++) {
+                for (int k = j + 1; k < N; k++) {
 
                     int tmp = arrN[i] + arrN[j] + arrN[k];
 
