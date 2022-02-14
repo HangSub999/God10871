8ce3d090df30e1da0d7db5e6421f8ef9ca2e21c3
HangSub999 <gkdtjq999@gmail.com>
2022-02-14 23:51:43 +0900

    22.02.14

--- src/SungJun_Misson/Baek0214_2798.java
+++ src/SungJun_Misson/Baek0214_2798.java
@@ -5,17 +5,38 @@ import java.util.Scanner;
 public class Baek0214_2798 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int N = sc.nextInt();
         int M = sc.nextInt();
-        int[] arrN = new int[n];
+        int[] arrN = new int[N];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             arrN[i] = sc.nextInt();
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+        int result = search(arrN, N, M);
+        System.out.println(result);
+    }
 
+    static int search(int[] arrN, int N, int M) {
+        int result = 0;
+
+        for (int i = 0; i < N - 2; i++) {
+
+            for (int j = 0; j < N - 1; j++) {
+
+                for (int k = 0; k < N; k++) {
+
+                    int tmp = arrN[i] + arrN[j] + arrN[k];
+
+                    if (M == tmp) {
+                        return tmp;
+                    }
+
+                    if (result < tmp && tmp < M) {
+                        result = tmp;
+                    }
+                }
             }
         }
+        return result;
     }
 }
