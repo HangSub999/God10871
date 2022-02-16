86a01e08506343e40d1819de7cbcca2a6974a68d
HangSub999 <gkdtjq999@gmail.com>
2022-02-16 16:11:45 +0900

    22.02.16

--- src/SungJun_Misson/Baek0215_10870.java
+++ src/SungJun_Misson/Baek0215_10870.java
@@ -7,15 +7,23 @@ public class Baek0215_10870 {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arrn = new int[n + 1];
+        int[] arr = new int[n + 1];
 
-        int sum = 0;
-
-        for (int i = 0; i < arrn.length - 2; i++) {
-            arrn[0] = 0;
-            arrn[1] = 1;
-            arrn[i + 2] = arrn[i] + arrn[i + 1];
+        if (n == 0) {
+            arr[0] = 0;
         }
-        System.out.println(arrn[n]);
+
+        if (n == 1) {
+            arr[1] = 1;
+        }
+
+
+        for (int i = 0; i < arr.length - 2; i++) {
+            arr[0] = 0;
+            arr[1] = 1;
+            arr[i + 2] = arr[i] + arr[i + 1];
+        }
+        System.out.println(arr[n]);
+
     }
 }
