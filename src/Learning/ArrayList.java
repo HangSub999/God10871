56de03c2ac5779e4cf1114c0230961a97ee07ca6
HangSub999 <gkdtjq999@gmail.com>
2022-02-17 22:23:25 +0900

    22.02.17 ArrlyList 구현

---
+++ src/Learning/ArrayList.java
@@ -0,0 +1,28 @@
+package Learning;
+
+public class ArrayList {
+    private int size = 0;
+    private Object[] elementData = new Object[100];
+
+    public boolean addLast(Object eement) {
+        elementData[size] = eement;
+        size++;
+        return true;
+    }
+
+    public boolean add(int index, Object element) {
+        for (int i = size - 1; i <= index; i--) {
+            elementData[i + 1] = elementData[i];
+        }
+        elementData[index] = element;
+        size++;
+        return true;
+    }
+
+    public boolean addFirst(Object element) {
+
+        return add(0, element);
+    }
+}
+
+
---
+++ src/Learning/ArrayListMain.java
@@ -0,0 +1,15 @@
+package Learning;
+
+public class ArrayListMain {
+    public static void main(String[] args) {
+
+        ArrayList numbers = new ArrayList();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addLast(40);
+        numbers.add(1, 15);
+        numbers.addFirst(5);
+
+    }
+}
