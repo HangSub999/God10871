e714ceb88e57611bdca7bbaacb506c9b7591f86e
HangSub999 <gkdtjq999@gmail.com>
2022-03-03 23:40:58 +0900

    22.03.03 arraylist 연습

---
+++ src/SungJun_Misson/Train0303_01.java
@@ -0,0 +1,28 @@
+package SungJun_Misson;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Train0303_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<String> str = new ArrayList<String>();
+
+        int siz = 0;
+        for (int i = 0; i < 5; i++) {
+            str.add(sc.next());
+
+            if (str.get(i).length() > siz) {
+                siz = str.get(i).length();
+            }
+
+        }
+        for (String cc : str) {
+            if (cc.length() == siz) {
+                System.out.println(cc);
+                break;
+            }
+        }
+    }
+}
