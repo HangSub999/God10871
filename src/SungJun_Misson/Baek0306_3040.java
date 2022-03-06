1b3a0ddaa85334b1baa79a6fc37ea0965b4f3018
HangSub999 <gkdtjq999@gmail.com>
2022-03-06 21:22:10 +0900

    22.03.06 다시풀어야 함

---
+++ src/SungJun_Misson/Baek0306_3040.java
@@ -0,0 +1,42 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0306_3040 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] a = new int[9];
+        int[] b = new int[7];
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            a[i] = sc.nextInt();
+            sum += a[i];
+        }
+        sum = sum - 100;
+        int aa = 0;
+        int bb = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                if (a[i] + a[j] == sum) {
+                    aa = a[i];
+                    bb = a[j];
+                }
+            }
+        }
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+
+                if (a[j] != aa && a[j] != bb) {
+                    b[i] = a[j];
+
+                }
+            }
+        }
+        for (int cc : b) {
+            System.out.println(cc);
+        }
+        System.out.println(aa + " " + bb);
+    }
+}
