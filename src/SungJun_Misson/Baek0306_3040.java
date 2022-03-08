9273913250be70a042471283a794f658f3b33d03
HangSub999 <gkdtjq999@gmail.com>
2022-03-08 21:59:24 +0900

    22.03.08 다시풀어야 함

--- src/SungJun_Misson/Baek0306_3040.java
+++ src/SungJun_Misson/Baek0306_3040.java
@@ -15,28 +15,28 @@ public class Baek0306_3040 {
             sum += a[i];
         }
         sum = sum - 100;
-        int aa = 2;
+        int aa = 0;
         int bb = 0;
         for (int i = 0; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
+            for (int j = i + 1; j < 9; j++) {
                 if (a[i] + a[j] == sum) {
                     aa = a[i];
                     bb = a[j];
                 }
             }
         }
-        for (int i = 0; i < b.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-
-                if (a[j] != aa && a[j] != bb) {
-                    b[i] = a[j];
-
+        for (int i = 0; i < 9; i++) {
+            if (a[i] != aa || a[i] != bb) {
+                for (int j = i + j; j < 6; j++) {
+                    b[j] =
                 }
+
             }
         }
+
         for (int cc : b) {
             System.out.println(cc);
         }
-        System.out.println(aa + " " + bb);
+
     }
 }
