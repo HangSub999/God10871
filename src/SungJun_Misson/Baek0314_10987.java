3058f33b4e5cc58dcff1f9e1867ade89b41d397d
HangSub999 <gkdtjq999@gmail.com>
2022-03-14 14:14:09 +0900

    22.03.14

---
+++ src/SungJun_Misson/Baek0314_10987.java
@@ -0,0 +1,23 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0314_10987 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] strChar = {'a', 'e', 'i', 'o', 'u'};
+
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char cha = str.charAt(i);
+            for (int j = 0; j < 5; j++) {
+                if (cha == strChar[j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
