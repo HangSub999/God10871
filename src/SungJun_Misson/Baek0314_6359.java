243c7934d7beae2fe7b7554c8591f8ffbd492467
HangSub999 <gkdtjq999@gmail.com>
2022-03-15 22:51:12 +0900

    22.03.15

---
+++ src/SungJun_Misson/Baek0314_6359.java
@@ -0,0 +1,13 @@
+package SungJun_Misson;
+
+import java.util.Scanner;
+
+public class Baek0314_6359 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int n = 0;
+
+    }
+}
