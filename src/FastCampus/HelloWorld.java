8f3d90d55d786083967adf8a44b18fabb3025765
HangSub999 <gkdtjq999@gmail.com>
2022-04-04 21:38:58 +0900

    feat: 패스트캠퍼스 1일차

---
+++ src/FastCampus/HelloWorld.java
@@ -0,0 +1,11 @@
+package FastCampus;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int javaScore1 = 100;
+        int javaScore2 = 88;
+
+        System.out.println("두 정수의 합" + javaScore1 + javaScore2);
+    }
+}
