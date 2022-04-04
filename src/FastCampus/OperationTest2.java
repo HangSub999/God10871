904796192b9d79500d330db7d154ec953ddadc47
HangSub999 <gkdtjq999@gmail.com>
2022-04-04 21:47:32 +0900

    feat: 패스트캠퍼스 1일차 증감연산자

---
+++ src/FastCampus/OperationTest2.java
@@ -0,0 +1,10 @@
+package FastCampus;
+
+public class OperationTest2 {
+    public static void main(String[] args) {
+        int score = 10;
+        System.out.println("score 값 : " + ++score);
+        System.out.println("score 값 : " + score++);
+        System.out.println("score 값 : " + score);
+    }
+}
