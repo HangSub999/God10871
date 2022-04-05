dedd8324b6d090dbf735d5b7b47003ca4a681457
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 19:48:26 +0900

    feat: if문을 이용하여 짝,홀 수 출력하기

---
+++ src/FastCampus/Exam1.java
@@ -0,0 +1,13 @@
+package FastCampus;
+
+public class Exam1 {
+    public static void main(String[] args) {
+        int num = 56;
+
+        if ((num % 2) == 0) {
+            System.out.println("짝수 입니다.");
+        } else if (num % 2 == 1) {
+            System.out.println("홀수 입니다.");
+        }
+    }
+}
