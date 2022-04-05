e7731851d44dcf0289a3af6b48eff68b7550349b
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 15:24:46 +0900

    feat: 클래스 구현 연습

--- src/FastCampus/Student.java
+++ src/FastCampus/Student.java
@@ -2,9 +2,19 @@ package FastCampus;
 
 public class Student {
 
-    int studentNumber;
-    String studentName;
-    int majorCode;
-    String majorNamee;
-    int grade;
+    public int studentID;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentID + "학번 학생의 이름은 " + studentName + "이고, 주소는" + address + "입니다.");
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
 }
