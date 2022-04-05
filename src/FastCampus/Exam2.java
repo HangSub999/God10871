df751164cdf899b63e2b4fbae5eed1bd5bc1b88c
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 19:47:42 +0900

    feat: 다른 변수를 이용하여 변수값 옮기기

---
+++ src/FastCampus/Exam2.java
@@ -0,0 +1,15 @@
+package FastCampus;
+
+public class Exam2 {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 44;
+        int tamp = 0;
+        tamp = num1;
+        num1 = num2;
+        num2 = tamp;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
