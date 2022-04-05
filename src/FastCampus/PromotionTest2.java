9962a5c6fac7f176044935e0e9782f4b150f1976
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 19:26:08 +0900

    feat: 명시적 형변환 연습

---
+++ src/FastCampus/PromotionTest2.java
@@ -0,0 +1,11 @@
+package FastCampus;
+
+public class PromotionTest2 {
+    public static void main(String[] args) {
+        double doubleAvg = 14.45;
+
+        int intAvg = (int) doubleAvg;
+
+        System.out.println("double로 변환된 평균값 : " + intAvg);
+    }
+}
