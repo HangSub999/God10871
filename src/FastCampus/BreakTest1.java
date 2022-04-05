d5942ea4bb33a091bb24d878ec0caf5bdc9de9a3
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 20:40:50 +0900

    feat: break문 연습

---
+++ src/FastCampus/BreakTest1.java
@@ -0,0 +1,13 @@
+package FastCampus;
+
+public class BreakTest1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;
+            }
+            System.out.println("i 변수 값 : " + i);
+        }
+
+    }
+}
