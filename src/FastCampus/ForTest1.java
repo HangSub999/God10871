7b819d71d63b84c0b3db68cd3343bce38c7528aa
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 20:16:11 +0900

    feat: for문 연습

---
+++ src/FastCampus/ForTest1.java
@@ -0,0 +1,15 @@
+package FastCampus;
+
+public class ForTest1 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println("1 ~ 10까지 홀수의 총 합 : " + sum);
+
+    }
+}
