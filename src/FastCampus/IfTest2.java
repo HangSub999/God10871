dab9719d7c1a4b0f4f7085f227f3efe161e25262
HangSub999 <gkdtjq999@gmail.com>
2022-04-05 19:37:17 +0900

    feat: 중첩if문 연습

---
+++ src/FastCampus/IfTest2.java
@@ -0,0 +1,15 @@
+package FastCampus;
+
+public class IfTest2 {
+    public static void main(String[] args) {
+        int javaScore = 89;
+
+        if (javaScore >= 60) {
+            if (javaScore >= 80) {
+                System.out.print("고득점으로 ");
+            }
+            System.out.println("합격입니다.");
+        }
+
+    }
+}
