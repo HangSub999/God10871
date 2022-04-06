dec5fd1653ed730da9c773d8e45ea6dc86116189
HangSub999 <gkdtjq999@gmail.com>
2022-04-06 16:02:05 +0900

    feat: BirthDay 클래스를 호출하여 출력하는 main 클래스

---
+++ src/FastCampus/BirthDayMain.java
@@ -0,0 +1,14 @@
+package FastCampus;
+
+public class BirthDayMain {
+    public static void main(String[] args) {
+
+        BirthDay data = new BirthDay();
+
+        data.setYear("2022");
+        data.setMonth(12);
+        data.setDay(32);
+
+        data.showDate();
+    }
+}
