85818ab12f6e45f56184ea5255efefc3329bfbef
HangSub999 <gkdtjq999@gmail.com>
2022-04-06 19:16:32 +0900

    feat: 참조변수null test

---
+++ src/FastCampus/NullTest1.java
@@ -0,0 +1,16 @@
+package FastCampus;
+
+public class NullTest1 {
+    public static void main(String[] args) {
+        int[] scoreList = {83, 76, 99, 100, 68};
+        System.out.println(scoreList);
+        scoreList = null;
+        System.out.println(scoreList);
+
+        String name = "김둘리";
+        System.out.println(name);
+        name = null;
+        System.out.println(name);
+
+    }
+}
