1573a724cf410e0407106a9e410104db5311a5fa
HangSub999 <gkdtjq999@gmail.com>
2022-04-07 14:16:00 +0900

    feat: bus 객체 생성

---
+++ src/FastCampus/ch14/Bus.java
@@ -0,0 +1,20 @@
+package FastCampus.ch14;
+
+public class Bus {
+    int busNumber;
+    int passengerCont;
+    int money;
+
+    public Bus(int busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public void take(int money) {
+        this.money += money;
+        this.passengerCont++;
+    }
+
+    public void showBusInfo() {
+        System.out.println(busNumber + "의 승객 수는 " + passengerCont + "명이고 수입은 " + money + "원 입니다.");
+    }
+}
