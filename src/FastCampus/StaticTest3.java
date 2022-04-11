b04ade715494d6c9851ac104177b4a7c83dddab2
HangSub999 <gkdtjq999@gmail.com>
2022-04-11 19:45:51 +0900

    feat: static 블럭 예시

---
+++ src/FastCampus/StaticTest3.java
@@ -0,0 +1,13 @@
+package FastCampus;
+
+public class StaticTest3 {
+
+    static {
+        System.out.println("static 블럭은 제일 처음 실행 됩니다.");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Main 메소드 실행");
+    }
+}
