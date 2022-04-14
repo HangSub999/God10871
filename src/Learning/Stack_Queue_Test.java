5ba629b461bd016dd5417893db7f1178a13492e0
johangseob <gkdtjq999@gmail.com>
2022-04-14 21:20:56 +0900

    feat: Stack,Queue 테스트

---
+++ src/Learning/Stack_Queue_Test.java
@@ -0,0 +1,34 @@
+package Learning;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Stack_Queue_Test {
+    public static void main(String[] args) {
+
+        Stack st = new Stack();
+        Queue queue = new LinkedList();
+
+        st.push(1);
+        st.push(2);
+        st.push(3);
+
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+
+        System.out.println("Stack-------");
+
+        while (!st.empty()) {
+            System.out.println(st.pop());
+        }
+
+        System.out.println("Queue---------");
+
+        while (!queue.isEmpty()) {
+
+            System.out.println(queue.poll());
+        }
+    }
+}
