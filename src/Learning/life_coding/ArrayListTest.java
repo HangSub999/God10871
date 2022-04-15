92c500e68984ce56d8fcd45d2cbd1a152bb02c9c
johangseob <gkdtjq999@gmail.com>
2022-04-15 20:51:24 +0900

    feat: ArrayListTest remove, get, size, indexOf, Iterator 연습

--- src/Learning/life_coding/ArrayListTest.java
+++ src/Learning/life_coding/ArrayListTest.java
@@ -1,6 +1,7 @@
 package Learning.life_coding;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListTest {
     public static void main(String[] args) {
@@ -16,5 +17,32 @@ public class ArrayListTest {
         numbers.add(1, 50);
         System.out.println("nadd(인덱스,값)");
         System.out.println(numbers);
+
+        numbers.remove(2);
+        System.out.println("remove(인덱스)");
+        System.out.println(numbers);
+
+        System.out.println("get(인덱스)");
+        System.out.println(numbers.get(2));
+
+        System.out.println("size");
+        System.out.println(numbers.size());
+
+        System.out.println("indexOf");
+        System.out.println(numbers.indexOf(10));
+
+        Iterator it = numbers.iterator();
+
+        System.out.println("Iterator");
+        while (it.hasNext()) {
+            int value = (int) it.next();
+            if (value == 30) {
+                it.remove();
+            }
+            System.out.println(value);
+        }
+        System.out.println(numbers);
+
+
     }
 }
