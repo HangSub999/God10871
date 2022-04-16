88293678463f95fcf6101560f00e599394a49fe1
johangseob <gkdtjq999@gmail.com>
2022-04-16 11:12:17 +0900

    feat: 3번을 누르면 랜덤숫자 저장해서 다시 가져오기 기능 추가

--- src/SungJon_Test/ReEnter/Main.java
+++ src/SungJon_Test/ReEnter/Main.java
@@ -1,5 +1,6 @@
 package SungJon_Test.ReEnter;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,12 +8,18 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
+        int[] storage = new int[3];
         boolean set = true;
         while (set) {
-            int count = 3;
-            int randam = (int) (Math.random() * 10) + 1;
-            System.out.println(randam);
 
+            int count = 3;
+            for (int i = 0; i < 3; i++) {
+                int randam = (int) (Math.random() * 10) + 1;
+                storage[i] = randam;
+                System.out.print(randam + " ");
+            }
+
+            System.out.println();
             System.out.println("번호를 입력해 주세요");
             int n = sc.nextInt();
             if (n == 1) {
@@ -24,7 +31,7 @@ public class Main {
             } else if (n >= 3) {
                 System.out.println("잘못 입력하셨습니다 다시 입력해 주세요");
                 if (count == 3) {
-
+                    System.out.println(Arrays.toString(storage));
                 }
 
                 continue;
