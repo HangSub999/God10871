fdefc4288d5b704d84bf261007b6f630102192f8
johangseob <gkdtjq999@gmail.com>
2022-04-17 08:40:17 +0900

    feat: do while 이용하여 재실행 기능 구현

--- src/SungJon_Test/ReEnter/Main.java
+++ src/SungJon_Test/ReEnter/Main.java
@@ -7,34 +7,39 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int[] storage = new int[3];
-        boolean set = true;
-        while (set) {
 
-            int count = 3;
+        boolean stop = true;
+
+        while (stop) {
+
+            System.out.println("----랜덤 숫자 생성----");
+
             for (int i = 0; i < 3; i++) {
-                int randam = (int) (Math.random() * 10) + 1;
-                storage[i] = randam;
-                System.out.print(randam + " ");
+                int random = (int) (Math.random() * 10) + 1;
+                storage[i] = random;
+                System.out.print(random + " ");
             }
-
             System.out.println();
-            System.out.println("번호를 입력해 주세요");
+
+
+            System.out.println("번호를 입력해 주세요 (1,2,3,4)");
             int n = sc.nextInt();
             if (n == 1) {
-                System.out.println("다시 입력");
-                continue;
-
+                System.out.println("-----다시 입력------");
             } else if (n == 2) {
-                break;
-            } else if (n >= 3) {
-                System.out.println("잘못 입력하셨습니다 다시 입력해 주세요");
-                if (count == 3) {
-                    System.out.println(Arrays.toString(storage));
-                }
-
-                continue;
+                stop = false;
+            } else if (n == 3) {
+                System.out.println(Arrays.toString(storage));
+                System.out.println("숫자 가져오기 완료!");
+            } else if (n == 4) {
+                System.out.println("----현재 숫자 출력----");
+                System.out.println(Arrays.toString(storage));
+                do {
+                    System.out.println("아무 키를 입력하면 재실행 합니다.");
+                } while (!sc.hasNext());
+            } else {
+                System.out.println("잘못 입력하셨습니다.");
             }
 
         }
