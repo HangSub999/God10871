a44e95d548138ddbb859d59b08892f4530c2e435
HangSub999 <gkdtjq999@gmail.com>
2022-05-02 19:40:32 +0900

    refactor: 랜덤숫자 중복 제거 수정

--- src/SungJon_Test/baseball_game/BaseballGame.java
+++ src/SungJon_Test/baseball_game/BaseballGame.java
@@ -1,6 +1,7 @@
 package SungJon_Test.baseball_game;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,75 +11,70 @@ public class BaseballGame {
         Scanner sc = new Scanner(System.in);
 
         List<Integer> userList = new ArrayList<>();
-        List<Integer> comList = new ArrayList<>();
+        LinkedHashSet<Integer> comList2 = new LinkedHashSet<>();
+
 
         boolean randomOnOf = true;
         boolean onOf = true;
 
         while (onOf) {
-
             if (randomOnOf) {
-                comList.add((int) (Math.random() * 9) + 1);
-                for (int i = 0; i < 2; i++) {
-                    int random = (int) (Math.random() * 9) + 1;
-                    if (comList.contains(random)) {
-                        i--;
+                while (comList2.size() != 3) {
+                    comList2.add((int) (Math.random() * 9) + 1);
+                }
+                List<Integer> comList = new ArrayList<>(comList2);
+
+                System.out.println(comList); //  랜덤숫자 정답 공개
+                System.out.print("숫자를 입력해주세요 : ");
+                int n = sc.nextInt();
+                userList.add(n / 100);
+                n = n % 100;
+                userList.add(n / 10);
+                userList.add(n % 10);
+
+                int strikeCount = 0;
+                for (int i = 0; i < comList.size(); i++) {
+                    if (comList.get(i).equals(userList.get(i))) {
+                        strikeCount++;
+                    }
+                }
+
+                int ballCount = 0;
+                for (Integer integer : userList) {
+                    for (Integer value : comList) {
+                        if (integer.equals(value)) {
+                            ballCount++;
+                        }
+                    }
+                }
+                ballCount -= strikeCount;
+
+                if (strikeCount == 3) {
+                    System.out.println("3 스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    int newGame = sc.nextInt();
+                    if (newGame == 2) {
+                        onOf = false;
                     } else {
-                        comList.add(random);
+                        comList2.clear();
+                        userList.clear();
+                        randomOnOf = true; // 재 시작시 랜덤숫자 초기화
                     }
-                }
-            }
-
-            System.out.println(comList); //  랜덤숫자 정답 공개
-            System.out.print("숫자를 입력해주세요 : ");
-            int n = sc.nextInt();
-            userList.add(n / 100);
-            n = n % 100;
-            userList.add(n / 10);
-            userList.add(n % 10);
-
-            int strikeCount = 0;
-            for (int i = 0; i < comList.size(); i++) {
-                if (comList.get(i).equals(userList.get(i))) {
-                    strikeCount++;
-                }
-            }
-
-            int ballCount = 0;
-            for (Integer integer : userList) {
-                for (Integer value : comList) {
-                    if (integer.equals(value)) {
-                        ballCount++;
-                    }
-                }
-            }
-            ballCount -= strikeCount;
-
-            if (strikeCount == 3) {
-                System.out.println("3 스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int newGame = sc.nextInt();
-                if (newGame == 2) {
-                    onOf = false;
                 } else {
-                    comList.clear();
+                    randomOnOf = false;
                     userList.clear();
-                    randomOnOf = true; // 재 시작시 랜덤숫자 초기화
-                }
-            } else {
-                randomOnOf = false;
-                userList.clear();
-                if (ballCount == 0 && strikeCount == 0) {
-                    System.out.println("포볼");
-                } else if (strikeCount < 3 && ballCount == 0) {
-                    System.out.println(strikeCount + " 스트라이크");
-                } else if (ballCount <= 3 && strikeCount == 0) {
-                    System.out.println(ballCount + "볼");
-                } else {
-                    System.out.printf("%d 스트라이크 %d볼%n", strikeCount, ballCount);
-                }
+                    if (ballCount == 0 && strikeCount == 0) {
+                        System.out.println("포볼");
+                    } else if (strikeCount < 3 && ballCount == 0) {
+                        System.out.println(strikeCount + " 스트라이크");
+                    } else if (ballCount <= 3 && strikeCount == 0) {
+                        System.out.println(ballCount + "볼");
+                    } else {
+                        System.out.printf("%d 스트라이크 %d볼%n", strikeCount, ballCount);
+                    }
 
+                }
             }
         }
     }
