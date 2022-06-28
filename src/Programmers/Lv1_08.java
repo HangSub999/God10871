0087fd28878d975b7d0f77dc56e7ea4f43f05560
HangSub999 <gkdtjq999@gmail.com>
2022-06-28 23:07:24 +0900

    feat: 한줄로 줄임

--- src/Programmers/Lv1_08.java
+++ src/Programmers/Lv1_08.java
@@ -1,10 +1,11 @@
 package Programmers;
 
 public class Lv1_08 {
+    public static void main(String[] args) {
+        System.out.println(solution(3));
+    }
     //짝수와 홀수
-    public String solution(int num) {
-        String answer = "";
-        answer = num % 2 == 0 ? "Even" : "Odd";
-        return answer;
+   static public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
     }
 }
