1f043b8c28855aab565edc9ff13719e5fe683324
HangSub999 <gkdtjq999@gmail.com>
2022-06-29 21:13:06 +0900

    feat: 쉽다

--- src/Programmers/Lv1_10.java
+++ src/Programmers/Lv1_10.java
@@ -3,11 +3,11 @@ package Programmers;
 public class Lv1_10 {
     //정수제곱근 판별
     public long solution(long n) {
-        long answer = (long)Math.sqrt(n);
+        long answer = (long) Math.sqrt(n);
 
-        if(answer*answer != n){
+        if (answer * answer != n) {
             return -1;
         }
-        return (1+answer)*(answer+1);
+        return (1 + answer) * (answer + 1);
     }
 }
