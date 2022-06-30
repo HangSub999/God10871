c7b05b2bc3864b1ca6997985f00c4a5b59422c3b
HangSub999 <gkdtjq999@gmail.com>
2022-06-30 19:42:03 +0900

    feat: 쉬움

---
+++ src/Programmers/Lv1_12.java
@@ -0,0 +1,21 @@
+package Programmers;
+
+public class Lv1_12 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+        String[] arry = str.split("");
+        int[] answer = new int[str.length()];
+
+        for (int i = 1; i <= str.length(); i++) {
+            int count = str.length() - i;
+            answer[i - 1] = Integer.parseInt(arry[count]);
+
+        }
+        return answer;
+    }
+}
+
