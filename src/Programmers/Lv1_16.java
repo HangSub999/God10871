dc761acfa97f21dccdc79f82b9a57edcaa2bb965
HangSub999 <gkdtjq999@gmail.com>
2022-07-01 23:07:46 +0900

    feat: 쉬움

---
+++ src/Programmers/Lv1_16.java
@@ -0,0 +1,10 @@
+package Programmers;
+
+public class Lv1_16 {
+    public static void main(String[] args) {
+        System.out.println(solution("-123"));
+    }
+   static public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
