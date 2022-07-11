5f1044a74d6d3bb0cc381569a95895d36158518b
HangSub999 <gkdtjq999@gmail.com>
2022-07-11 20:11:36 +0900

    feat: 히힛 쉽다

---
+++ src/Programmers/Lv1_29.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+public class Lv1_29 {
+    public static void main(String[] args) {
+
+        System.out.println(solution(3, 20, 4));
+    }
+
+    // 부족한 금액 계산기
+    static public long solution(int price, int money, int count) {
+        int[] tmp = new int[count];
+        int total = 0;
+        for (int i = 1; i <= count; i++) {
+            tmp[i - 1] = price * i;
+        }
+        for (int sum : tmp) {
+            total += sum;
+        }
+
+        if (money > total) {
+            return 0;
+        } else {
+            return total - money;
+        }
+
+    }
+}
