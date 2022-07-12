783a3795fe9f6991658c844ab395a8f528649137
HangSub999 <gkdtjq999@gmail.com>
2022-07-12 15:50:08 +0900

    feat: 드디어 풀었군

--- src/Programmers/Lv1_13.java
+++ src/Programmers/Lv1_13.java
@@ -4,28 +4,31 @@ import java.util.Arrays;
 
 public class Lv1_13 {
     public static void main(String[] args) {
-        int[][] test = {{10,7},{12,3},{8,15},{14,7},{5,15}};
+        int[][] test = {{60, 50}, {30, 70}, {60, 30}, {80, 40}};
         System.out.println(solution(test));
 
     }
-
+//최소 직사각형
     static public int solution(int[][] sizes) {
-        int[][] max = new int[1][2];
-        for (int i = 0; i < sizes.length; i++) {
-            for (int j = 0; j < sizes[0].length; j++) {
-                if (max[0][0] < sizes[i][0]) {
-                    max[0][0] = sizes[i][0];
+        int max = 0;
+        for (int[] size : sizes) {
+            for (int i : size) {
+                if (i > max) {
+                    max = i;
                 }
             }
         }
+
+        int[] min = new int[sizes.length];
         for (int i = 0; i < sizes.length; i++) {
-            for (int j = 0; j < sizes[1].length; j++) {
-                if (max[0][1] < sizes[i][1]) {
-                    max[0][1] = sizes[i][1];
+            for (int j = 0; j < sizes[i].length - 1; j++) {
+                min[i] = sizes[i][j];
+                if (sizes[i][j + 1] < min[i]) {
+                    min[i] = sizes[i][j + 1];
                 }
             }
         }
-        System.out.println(Arrays.deepToString(max));
-        return max[0][0] * max[0][1];
+        Arrays.sort(min);
+        return max * min[min.length - 1];
     }
 }
