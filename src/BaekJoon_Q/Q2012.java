4f6e8c986cf006f1ea9e82e799aa778f0cfe2585
HangSub999 <gkdtjq999@gmail.com>
2022-08-16 20:51:53 +0900

    feat: 탐욕 알고리즘

--- src/BaekJoon_Q/Q2012.java
+++ src/BaekJoon_Q/Q2012.java
@@ -3,26 +3,18 @@ package BaekJoon_Q;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Q11399 {
-    public static void
-    main(String[] args) {
+public class Q2012 {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] num = new int[scanner.nextInt()];
-        int[] sum = new int[num.length];
         for (int i = 0; i < num.length; i++) {
             num[i] = scanner.nextInt();
         }
         Arrays.sort(num);
+        long count = 0;
         for (int i = 0; i < num.length; i++) {
-            if (i != 0) {
-                num[i] += num[i - 1];
-            }
-            sum[i] = num[i];
+            count += Math.abs(i + 1 - num[i]);
         }
-        int total = 0;
-        for (int j : sum) {
-            total += j;
-        }
-        System.out.println(total);
+        System.out.println(count);
     }
 }
